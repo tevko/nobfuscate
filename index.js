3939ec7ed09187f52fda1c7d31988f2d3f1c89cb
Tim Evko <timevko@gmail.com>
2018-01-08 21:15:59 -0500

    get all js files in directory

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//not yet
+const fs = require('fs');
+const path = require('path');
+const BASE_DIR = process.argv[process.argv.length - 1];
+
+const walkSync = (dir, filelist = []) => {
+    fs.readdirSync(dir).forEach(file => {
+        filelist = fs.statSync(path.join(dir, file)).isDirectory()
+        ? walkSync(path.join(dir, file), filelist)
+        : filelist.concat(path.join(dir, file));
+    });
+    return filelist;
+};
+
+const onlyJS = walkSync(BASE_DIR).filter(f => f.split('.js')[1] === '');
+
+console.log(onlyJS);
