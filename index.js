8e463063c5dd066172aab20ad6e42ab5fbe6bde9
Tim Evko <timevko@gmail.com>
2018-01-09 20:47:28 -0500

    enable proper console output

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const fs = require('fs');
 const path = require('path');
 const BASE_DIR = process.argv[process.argv.length - 1];
 
-const walkSync = (dir, filelist = []) => {
+const maliciousChecks = ['<<', '~', '""', 'fromCharCode', '0x', '\\x', '][', 'eval', '~~', 'charCodeAt', 'z1', 'new RegExp', '()+', '\'\'', 'v5', 'v8', 'v0', '>>>', '^', '|', '>>', 'new Function', 'unescape', 'atob'];
+
+const getAllJsInDirectory = (dir, filelist = []) => {
     fs.readdirSync(dir).forEach(file => {
-        filelist = fs.statSync(path.join(dir, file)).isDirectory()
-        ? walkSync(path.join(dir, file), filelist)
-        : filelist.concat(path.join(dir, file));
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            filelist = getAllJsInDirectory(path.join(dir, file), filelist);
+        } else if (file.split('.js')[1] === '') {
+            filelist = filelist.concat(path.join(dir, file));
+        }
     });
     return filelist;
 };
 
-const onlyJS = walkSync(BASE_DIR).filter(f => f.split('.js')[1] === '');
+const onlyJS = getAllJsInDirectory(BASE_DIR);
 
-console.log(onlyJS);
+onlyJS.forEach(filepath => {
+    const data = fs.readFileSync(filepath, 'utf8');
+    let points = 0;
+    let redFlags = [];
+    maliciousChecks.forEach((badThing) => {
+		if (data.toString().indexOf(badThing) !== -1) {
+			points += 1;
+			redFlags.push(badThing);
+        }
+    });
+    if (points > 3) {
+        console.log(`${filepath}: score: ${points}, redflags: ${redFlags}`);
+    }
+});
